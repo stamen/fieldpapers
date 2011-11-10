edeeda2402a86f3294e093264f1cc3e146bbe364
Michal Migurski <migurski@domU-12-31-38-04-20-A5.compute-1.internal>
2011-11-10 02:49:34 +0000

    Commented and cleaned-up GeoJSON parsing code

--- site/www/compose-print.php
+++ site/www/compose-print.php
@@ -9,6 +9,10 @@
 
     $json = json_decode(file_get_contents('php://input'), true);
     
+    //
+    // Perform a few basic idiot-checks to verify that we're looking at GeoJSON.
+    //
+    
     if(!is_array($json))
     {
         die_with_code(400, 'Bad JSON input');
@@ -19,27 +23,8 @@
         die_with_code(400, 'Bad GeoJSON input');
     }
     
-    $p = $json['properties'];
-
-    $paper_size = (is_array($p) && isset($p['paper_size'])) ? $p['paper_size'] : 'letter';
-    $orientation = (is_array($p) && isset($p['orientation'])) ? $p['orientation'] : 'portrait';
-    
-    switch("{$paper_size}, {$orientation}")
-    {
-        case ('letter, portrait'):
-            $paper_aspect = PAPER_PORTRAIT_LTR_WIDTH / PAPER_PORTRAIT_LTR_HEIGHT;
-            break;
-        
-        default:
-            die_with_code(500, "I don't know how to do this yet, sorry.");
-    }
-    
     foreach($json['features'] as $feature)
     {
-        //
-        // Do some basic idiot-checks
-        //
-        
         if(!is_array($feature))
         {
             die_with_code(400, 'Bad JSON input');
@@ -49,6 +34,36 @@
         {
             die_with_code(400, 'Bad GeoJSON input');
         }
+    }
+
+    //
+    // Move on to the actual business of converting GeoJSON to an atlas.
+    // Start with a global paper size and orientation for the full document.
+    //
+    
+    $p = $json['properties'];
+
+    $paper_size = (is_array($p) && isset($p['paper_size'])) ? $p['paper_size'] : 'letter';
+    $orientation = (is_array($p) && isset($p['orientation'])) ? $p['orientation'] : 'portrait';
+    $paper_type = "{$orientation}, {$paper_size}";
+    
+    if($paper_type == 'portrait, letter') {
+        $paper_aspect = PAPER_PORTRAIT_LTR_WIDTH / PAPER_PORTRAIT_LTR_HEIGHT;
+    
+    } else {
+        die_with_code(500, "I don't know how to do this yet, sorry.");
+    }
+    
+    //
+    // Iterate over each feature and determine an appropriate extent and zoom.
+    // Each feature in the GeoJSON becomes a single page in the atlas.
+    //
+    
+    foreach($json['features'] as $feature)
+    {
+        //
+        // Check the properties for a provider and explicit zoom.
+        //
         
         $p = $feature['properties'];
 
@@ -59,65 +74,61 @@
         $explicit_zoom = is_array($p) && is_numeric($p['zoom']);
         $zoom = $explicit_zoom ? intval($p['zoom']) : 16;
         
+        //
+        // Determine extent based on geometry type and zoom level.
+        //
+        
         $extent = null;
         
-        switch($feature['geometry']['type'])
-        {
-            case 'Point':
-                $coords = $feature['geometry']['coordinates'];
-                $center = new MMaps_Location($coords[1], $coords[0]);
-                $dimensions = new MMaps_Point(1200, 1200); // aims for between 150-200dpi
-                
-                if($paper_aspect >= 1) {
-                    // paper is wide
-                    $dimensions->x = floor($dimensions->x * $paper_aspect);
-                
-                } else {
-                    // paper is tall
-                    $dimensions->y = floor($dimensions->y / $paper_aspect);
-                }
+        if($feature['geometry']['type'] == 'Point') {
 
-                // make a temporary map with the correct center and aspect ratio
-                $_mmap = MMaps_mapByCenterZoom($provider, $center, $zoom, $dimensions);
-
-                // make a new extent with the corners of the map above.
-                $extent = array($_mmap->pointLocation(new MMaps_Point(0, 0)),
-                                $_mmap->pointLocation($mmap->dimensions));
-                
-                break;
+            $coords = $feature['geometry']['coordinates'];
+            $center = new MMaps_Location($coords[1], $coords[0]);
+            $dimensions = new MMaps_Point(1200, 1200); // aims for between 150-200dpi
             
-            case 'Polygon':
-                $coords = $feature['geometry']['coordinates'][0];
-                
-                list($minlon, $minlat) = array($coords[0][0], $coords[0][1]);
-                list($maxlon, $maxlat) = array($coords[0][0], $coords[0][1]);
-                
-                foreach($coords as $coord)
-                {
-                    $minlon = min($minlon, $coord[0]);
-                    $minlat = min($minlat, $coord[1]);
-                    $maxlon = max($maxlon, $coord[0]);
-                    $maxlat = max($maxlat, $coord[1]);
-                }
-                
-                $extent = array(new MMaps_Location($minlat, $minlon),
-                                new MMaps_Location($maxlat, $maxlon));
-
-                break;
+            if($paper_aspect >= 1) {
+                // paper is wide
+                $dimensions->x = floor($dimensions->x * $paper_aspect);
             
-            default:
-                die_with_code(500, "I don't know how to do this yet, sorry.");
+            } else {
+                // paper is tall
+                $dimensions->y = floor($dimensions->y / $paper_aspect);
+            }
+
+            // make a temporary map with the correct center and aspect ratio
+            $_mmap = MMaps_mapByCenterZoom($provider, $center, $zoom, $dimensions);
+
+            // make a new extent with the corners of the map above.
+            $extent = array($_mmap->pointLocation(new MMaps_Point(0, 0)),
+                            $_mmap->pointLocation($mmap->dimensions));
+        
+        } elseif($feature['geometry']['type'] == 'Polygon') {
+
+            $coords = $feature['geometry']['coordinates'][0];
+            
+            list($minlon, $minlat) = array($coords[0][0], $coords[0][1]);
+            list($maxlon, $maxlat) = array($coords[0][0], $coords[0][1]);
+            
+            foreach($coords as $coord)
+            {
+                $minlon = min($minlon, $coord[0]);
+                $minlat = min($minlat, $coord[1]);
+                $maxlon = max($maxlon, $coord[0]);
+                $maxlat = max($maxlat, $coord[1]);
+            }
+            
+            $extent = array(new MMaps_Location($minlat, $minlon),
+                            new MMaps_Location($maxlat, $maxlon));
+        
+        } else {
+            die_with_code(500, "I don't know how to do this yet, sorry.");
         }
         
-        switch(true)
-        {
-            case (is_array($extent) && $explicit_zoom):
-                $mmap = MMaps_mapByExtentZoom($provider, $extent[0], $extent[1], $zoom);
-                break;
-            
-            default:
-                die_with_code(500, "I don't know how to do this yet, sorry.");
-        }
+        //
+        // If we got this far, we know we have a meaningful zoom and extent for this page.
+        //
+
+        $mmap = MMaps_mapByExtentZoom($provider, $extent[0], $extent[1], $zoom);
         
         print_r($mmap);
     }
