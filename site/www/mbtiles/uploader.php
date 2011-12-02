779e4d228d25b1396ac0634f441d310a437f32bb
Michael Lawrence Evans <mevans@stamen.com>
2011-12-01 17:24:22 -0800

    Cleaning up code related to mbtiles support

--- site/www/mbtiles/uploader.php
+++ site/www/mbtiles/uploader.php
@@ -1,18 +1,15 @@
 <?php
-	//Upload mbtiles to the server.
-	
-	$target_mbtiles_folder = "uploaded_mbtiles/";
-	
-	$target_mbtiles_path = $target_mbtiles_folder . basename($_FILES['uploaded_mbtiles']['name']);
-	
-	//echo $target_mbtiles_path;
-	//exit();
-	
-	if(move_uploaded_file($_FILES['uploaded_mbtiles']['tmp_name'], $target_mbtiles_path)){
-		echo basename($_FILES['uploaded_mbtiles']['name']) . " has been successfully uploaded";
-	} else {
-		echo "Upload of " . basename($_FILES['uploaded_mbtiles']['name']) . " was unsuccessful.";
-	}
-	
-	//File is now sitting on the server at uploaded_tiles/some_name.mbtiles.
+    //Upload mbtiles to the server.
+    
+    $target_mbtiles_folder = "uploaded_mbtiles/";
+    
+    $target_mbtiles_path = $target_mbtiles_folder . basename($_FILES['uploaded_mbtiles']['name']);
+    
+    if(move_uploaded_file($_FILES['uploaded_mbtiles']['tmp_name'], $target_mbtiles_path)){
+        echo basename($_FILES['uploaded_mbtiles']['name']) . " has been successfully uploaded";
+    } else {
+        echo "Upload of " . basename($_FILES['uploaded_mbtiles']['name']) . " was unsuccessful.";
+    }
+    
+    //File is now sitting on the server at uploaded_tiles/some_name.mbtiles.
 ?>
