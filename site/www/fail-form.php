ad2e4fe2858f7324cf22dd220f1fa854545c0d0b
Michal Migurski <migurski@domU-12-31-38-04-20-A5.compute-1.internal>
2012-01-25 00:46:49 +0000

    Added scan finish and fail callbacks

---
+++ site/www/fail-form.php
@@ -0,0 +1,51 @@
+<?php
+   /**
+    * 
+    */
+
+    ini_set('include_path', ini_get('include_path').PATH_SEPARATOR.'../lib');
+    require_once 'init.php';
+    require_once 'data.php';
+    require_once 'lib.auth.php';
+
+    $form_id = $_GET['id'] ? $_GET['id'] : null;
+    
+    list($user_id, $language) = read_userdata($_COOKIE['visitor'], $_SERVER['HTTP_ACCEPT_LANGUAGE']);
+    
+    enforce_master_on_off_switch($language);
+
+    /**** ... ****/
+    
+    $dbh =& get_db_connection();
+    
+    if($user_id)
+        $user = get_user($dbh, $user_id);
+
+    if($user)
+        setcookie('visitor', write_userdata($user['id'], $language), time() + 86400 * 31);
+    
+    $form = get_form($dbh, $form_id);
+    
+    if(!$form)
+    {
+        die_with_code(400, "I don't know that form");
+    }
+    
+    if($_SERVER['REQUEST_METHOD'] == 'POST')
+    {
+        if($_POST['password'] != API_PASSWORD)
+            die_with_code(401, 'Sorry, bad password');
+        
+        $dbh->query('START TRANSACTION');
+        
+        add_log($dbh, "Failing form {$form['id']}");
+
+        fail_form($dbh, $form['id'], 1);
+
+        $dbh->query('COMMIT');
+    }
+    
+    header('HTTP/1.1 200');
+    echo "OK\n";
+
+?>
