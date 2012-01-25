2d604234aff3002d5f9630172482a31c2ef45a50
Michal Migurski <migurski@domU-12-31-38-04-20-A5.compute-1.internal>
2012-01-25 00:46:49 +0000

    Added scan finish and fail callbacks

--- site/lib/lib.forms.php
+++ site/lib/lib.forms.php
@@ -161,4 +161,31 @@
         return get_form_field($dbh, $field['form_id'], $field['name']);
     }
     
+    function finish_form(&$dbh, $form_id)
+    {
+        $q = sprintf('UPDATE forms SET parsed = NOW() WHERE id = %s',
+                     $dbh->quoteSmart($form_id));
+
+        error_log(preg_replace('/\s+/', ' ', $q));
+
+        $res = $dbh->query($q);
+        
+        if(PEAR::isError($res))
+            die_with_code(500, "{$res->message}\n{$q}\n");
+    }
+    
+    function fail_form(&$dbh, $form_id, $failure=1)
+    {
+        $q = sprintf('UPDATE forms SET failed = %s WHERE id = %s',
+                     $dbh->quoteSmart($failure),
+                     $dbh->quoteSmart($form_id));
+
+        error_log(preg_replace('/\s+/', ' ', $q));
+
+        $res = $dbh->query($q);
+        
+        if(PEAR::isError($res))
+            die_with_code(500, "{$res->message}\n{$q}\n");
+    }
+    
 ?>
---
+++ site/www/finish-form.php
@@ -0,0 +1,76 @@
+<?php
+
+    header('HTTP/1.1 500');
+
+?><?php
+   /**
+    * Display page for a single print with a given ID.
+    *
+    * When this page receives a POST request, it's probably from compose.py
+    * (check the API_PASSWORD) with new information on print components for
+    * building into a new PDF.
+    */
+
+    ini_set('include_path', ini_get('include_path').PATH_SEPARATOR.'../lib');
+    require_once 'init.php';
+    require_once 'data.php';
+    require_once 'lib.auth.php';
+    require_once 'lib.forms.php';
+
+    $form_id = $_GET['id'] ? $_GET['id'] : null;
+    
+    list($user_id, $language) = read_userdata($_COOKIE['visitor'], $_SERVER['HTTP_ACCEPT_LANGUAGE']);
+    
+    enforce_master_on_off_switch($language);
+
+    /**** ... ****/
+    
+    $dbh =& get_db_connection();
+    
+    if($user_id)
+        $user = get_user($dbh, $user_id);
+
+    if($user)
+        setcookie('visitor', write_userdata($user['id'], $language), time() + 86400 * 31);
+    
+    $form = get_form($dbh, $form_id);
+    
+    if(!$form)
+    {
+        die_with_code(400, "I don't know that form");
+    }
+    
+    if($_SERVER['REQUEST_METHOD'] == 'POST')
+    {
+        if($_POST['password'] != API_PASSWORD)
+            die_with_code(401, 'Sorry, bad password');
+        
+        $dbh->query('START TRANSACTION');
+        
+        foreach($_POST['fields'] as $_field)
+        {
+            $field = add_form_field($dbh, $form['id'], $_field['name']);
+            
+            if(!$field)
+            {
+                die_with_code(400, "I don't know that field");
+            }
+        
+            $field['type'] = $_field['type'];
+            $field['label'] = $_field['label'];
+            set_form_field($dbh, $field);
+        }
+
+        $form['http_method'] = $_POST['http_method'];
+        $form['action_url'] = $_POST['action_url'];
+        set_form($dbh, $form);
+
+        finish_form($dbh, $form['id']);
+
+        $dbh->query('COMMIT');
+    }
+    
+    header('HTTP/1.1 200');
+    echo "OK\n";
+
+?>
