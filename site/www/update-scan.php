a462261398bf0d28ade46f5a604b2c90a3187382
Michal Migurski <migurski@domU-12-31-38-04-20-A5.compute-1.internal>
2012-02-03 23:45:06 +0000

    Sending unused scan progress updates to update-scan.php

---
+++ site/www/update-scan.php
@@ -0,0 +1,35 @@
+<?php
+
+    ini_set('include_path', ini_get('include_path').PATH_SEPARATOR.'../lib');
+    require_once 'init.php';
+    require_once 'data.php';
+    require_once 'lib.auth.php';
+
+    enforce_master_on_off_switch($language);
+    enforce_api_password($_POST['password']);
+
+    /**** ... ****/
+    
+    $dbh =& get_db_connection();
+
+    $scan_id = $_GET['id'] ? $_GET['id'] : null;
+    $scan = get_scan($dbh, $scan_id);
+    
+    if(!$scan)
+    {
+        die_with_code(400, "I don't know that scan");
+    }
+    
+    if($progress = $_POST['progress'])
+    {
+        $dbh->query('START TRANSACTION');
+        
+        error_log("Update scan {$scan['id']} with {$progress}");
+
+        $dbh->query('COMMIT');
+    }
+    
+    header('HTTP/1.1 200');
+    echo "OK\n";
+
+?>
