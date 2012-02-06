9cf52a48f83367b15f41c6b8c3eb16d74d10d1e4
Michael Lawrence Evans <mevans@stamen.com>
2012-02-06 23:21:27 +0000

    Removing unused code

--- site/www/forms.php
+++ site/www/forms.php
@@ -4,29 +4,16 @@
     */
 
     ini_set('include_path', ini_get('include_path').PATH_SEPARATOR.'../lib');
-    ini_set('include_path', ini_get('include_path').PATH_SEPARATOR.'/usr/home/migurski/pear/lib');
     require_once 'init.php';
     require_once 'data.php';
     require_once 'lib.auth.php';
     require_once 'lib.forms.php';
-    
-    //list($user_id, $language) = read_userdata($_COOKIE['visitor'], $_SERVER['HTTP_ACCEPT_LANGUAGE']);
-    
-    //enforce_master_on_off_switch($language);
 
     /**** ... ****/
     
     session_start();
     $dbh =& get_db_connection();
     remember_user($dbh);
-    
-    /*
-    if($user_id)
-        $user = get_user($dbh, $user_id);
-
-    if($user)
-        setcookie('visitor', write_userdata($user['id'], $language), time() + 86400 * 31);
-    */
     
     $pagination = array('page' => $_GET['page'], 'perpage' => $_GET['perpage']);
     
