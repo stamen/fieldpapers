92d4bb8022290f556e43a6ab1ff78ff9f015e1d1
Michal Migurski <migurski@ip-10-72-73-195.ec2.internal>
2012-02-07 21:20:54 +0000

    Expanded scan note functions and created new add-note.php endpoint for them

--- site/lib/lib.scans.php
+++ site/lib/lib.scans.php
@@ -2,11 +2,31 @@
 
     require_once 'data.php';
     
+    function get_scan_note(&$dbh, $scan_id, $note_number)
+    {
+        $q = sprintf("SELECT scan_id, note_number, note,
+                             latitude, longitude, geometry
+                      FROM notes
+                      WHERE scan_id = %s
+                        AND note_number = %s",
+                     $dbh->quoteSmart($scan_id),
+                     $dbh->quoteSmart($note_number));
+    
+        $res = $dbh->query($q);
+        
+        if(PEAR::isError($res)) 
+            die_with_code(500, "{$res->message}\n{$q}\n");
+
+        $row = $res->fetchRow(DB_FETCHMODE_ASSOC);
+        
+        return $row;
+    }
+    
     function get_scan_notes(&$dbh, $scan_id, $page)
     {
         list($count, $offset, $perpage, $page) = get_pagination($page);
         
-        $q = sprintf('SELECT scan_id, number, note,
+        $q = sprintf('SELECT scan_id, note_number, note,
                              latitude, longitude, geometry
                       FROM scan_notes
                       WHERE %s
@@ -27,6 +47,63 @@
             $rows[] = $row;
         
         return $rows;
+    }
+    
+    function add_scan_note(&$dbh, $scan_id, $note_number)
+    {
+        $q = sprintf('INSERT INTO notes
+                      SET scan_id = %s, note_number = %d',
+                     $dbh->quoteSmart($scan_id),
+                     $dbh->quoteSmart($note_number));
+
+        error_log(preg_replace('/\s+/', ' ', $q));
+
+        $res = $dbh->query($q);
+        
+        if(PEAR::isError($res)) 
+        {
+            die_with_code(500, "{$res->message}\n{$q}\n");
+        }
+        
+        return get_scan_note($dbh, $scan_id, $note_number);
+    }
+    
+    function set_scan_note(&$dbh, $note)
+    {
+        $old_note = get_scan_note($dbh, $note['scan_id'], $note['note_number']);
+        
+        if(!$old_note)
+            return false;
+
+        $update_clauses = array();
+
+        foreach(array('latitude', 'longitude', 'note', 'geometry', 'user_id') as $field)
+            if(!is_null($note[$field]))
+                if($note[$field] != $old_note[$field])
+                    $update_clauses[] = sprintf('%s = %s', $field, $dbh->quoteSmart($note[$field]));
+
+        if(empty($update_clauses)) {
+            error_log("skipping note {$note['scan_id']}/{$note['note_number']} update since there's nothing to change");
+
+        } else {
+            $update_clauses = join(', ', $update_clauses);
+            
+            $q = sprintf('UPDATE notes SET %s
+                          WHERE scan_id = %s
+                            AND note_number = %s',
+                         $update_clauses,
+                         $dbh->quoteSmart($note['scan_id']),
+                         $dbh->quoteSmart($note['note_number']));
+            
+            error_log(preg_replace('/\s+/', ' ', $q));
+    
+            $res = $dbh->query($q);
+            
+            if(PEAR::isError($res))
+                die_with_code(500, "{$res->message}\n{$q}\n");
+        }
+
+        return get_scan_note($dbh, $note['scan_id'], $note['note_number']);
     }
     
 ?>
---
+++ site/www/add-note.php
@@ -0,0 +1,44 @@
+<?php
+
+    ini_set('include_path', ini_get('include_path').PATH_SEPARATOR.'../lib');
+    ini_set('include_path', ini_get('include_path').PATH_SEPARATOR.'/usr/home/migurski/pear/lib');
+    require_once 'init.php';
+    require_once 'data.php';
+    require_once 'lib.auth.php';
+    require_once 'lib.scans.php';
+    
+    enforce_master_on_off_switch($language);
+
+    session_start();
+    $dbh =& get_db_connection();
+    remember_user($dbh);
+    
+    $user = cookied_user($dbh);
+    $user_id = $user['id'];
+    
+    /**** ... ****/
+    
+    if($scan = get_scan($dbh, $_POST['scan_id']) && $_POST['note'] && $_POST['lat'] && $_POST['lon'])
+    {
+        $dbh->query('START TRANSACTION');
+
+        $note_number = 1;
+        
+        foreach(get_scan_notes($dbh, $scan['id']) as $note)
+        {
+            $note_number = max($note_number, $note['note_numer'] + 1);
+        }
+        
+        $note = add_scan_note(&$dbh, $scan_id, $note_number);
+        
+        $note['note'] = $_POST['note'];
+        $note['latitude'] = $_POST['lat'];
+        $note['longitude'] = $_POST['lon'];
+        $note['geometry'] = sprintf('POINT(%.6f %.6f)', $_POST['lon'], $_POST['lat']);
+        
+        set_scan_note(&$dbh, $note);
+        
+        $dbh->query('COMMIT');
+    }
+
+?>
