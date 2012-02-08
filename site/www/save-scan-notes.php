2b801494d60b296e982483b91296d4aff07459a0
Michael Lawrence Evans <mevans@stamen.com>
2012-02-08 23:11:43 +0000

    Adding ability to save multiple notes at a time

---
+++ site/www/save-scan-notes.php
@@ -0,0 +1,57 @@
+<?php
+
+    ini_set('include_path', ini_get('include_path').PATH_SEPARATOR.'../lib');
+    ini_set('include_path', ini_get('include_path').PATH_SEPARATOR.'/usr/home/migurski/pear/lib');
+    require_once 'init.php';
+    require_once 'data.php';
+    require_once 'lib.auth.php';
+    require_once 'lib.scans.php';
+    
+    enforce_master_on_off_switch($language);
+
+    session_start();
+    $dbh =& get_db_connection();
+    remember_user($dbh);
+    
+    $user = cookied_user($dbh);
+    $user_id = $user['id'];
+    
+    /**** ... ****/
+    
+    header('content-type: text/plain');
+    //print_r($_POST['marker']);
+    
+    foreach($_POST['marker'] as $key => $marker)
+    {
+        //print_r($key);
+        //print_r($marker);
+        
+        // Add each note
+        
+        if(($scan = get_scan($dbh, $marker['scan_id'])) && $marker['note'] && $marker['lat'] && $marker['lon'])
+        {
+            $dbh->query('START TRANSACTION');
+            
+            $note_number = 1;
+            
+            foreach(get_scan_notes($dbh, $scan['id']) as $note)
+            {
+                $note_number = max($note_number, $note['note_number'] + 1);
+            }
+            
+            $note = add_scan_note($dbh, $scan['id'], $note_number);
+            
+            $note['note'] = $marker['note'];
+            $note['latitude'] = $marker['lat'];
+            $note['longitude'] = $marker['lon'];
+            $note['geometry'] = sprintf('POINT(%.6f %.6f)', $marker['lon'], $marker['lat']);
+            
+            set_scan_note($dbh, $note);
+            
+            $dbh->query('COMMIT');
+        }
+        
+        header('Location: http://'.get_domain_name().get_base_dir().'/scan.php?id='.urlencode($scan['id']));
+    }
+    
+?>
