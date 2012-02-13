921d9fbdcf11fc12bb59b634cc2d61dcd31e7c36
Michael Lawrence Evans <mevans@stamen.com>
2012-02-13 23:43:49 +0000

    Cleaning up about page

--- site/www/about.php
+++ site/www/about.php
@@ -4,12 +4,9 @@
     */
 
     ini_set('include_path', ini_get('include_path').PATH_SEPARATOR.'../lib');
-    ini_set('include_path', ini_get('include_path').PATH_SEPARATOR.'/usr/home/migurski/pear/lib');
     require_once 'init.php';
     require_once 'data.php';
     require_once 'lib.auth.php';
-    
-    //list($user_id, $language) = read_userdata($_COOKIE['visitor'], $_SERVER['HTTP_ACCEPT_LANGUAGE']);
     
     enforce_master_on_off_switch($language);
 
@@ -18,15 +15,7 @@
     session_start();
     $dbh =& get_db_connection();
     remember_user($dbh);
-    
-    /*
-    if($user_id)
-        $user = get_user($dbh, $user_id);
-
-    if($user)
-        setcookie('visitor', write_userdata($user['id'], $language), time() + 86400 * 31);
-    */
-    
+        
     $sm = get_smarty_instance();
     $sm->assign('language', $language);
     
