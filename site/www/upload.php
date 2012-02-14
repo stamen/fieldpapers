6a6d1fe438c653d8109643d92fa641d430ef9b25
Michael Lawrence Evans <mevans@stamen.com>
2012-02-14 20:10:09 +0000

    Using cookied_user function instead of using Session variable, removing language variable, and creating enforce/session block

--- site/www/upload.php
+++ site/www/upload.php
@@ -9,15 +9,15 @@
 
     require_once '../lib/lib.everything.php';
     
-    $language = $_SERVER['HTTP_ACCEPT_LANGUAGE'];
-    enforce_master_on_off_switch($language);
-
-    /**** ... ****/ 
+    enforce_master_on_off_switch($_SERVER['HTTP_ACCEPT_LANGUAGE']);
+    
     session_start();
     $dbh =& get_db_connection();
     remember_user($dbh);
-        
-    $user = get_user($dbh, $_SESSION['user']['id']);    
+
+    /**** ... ****/ 
+    
+    $user = cookied_user($dbh);
     
     $dbh->query('START TRANSACTION');
     $scan = add_scan($dbh, $user['id']);
