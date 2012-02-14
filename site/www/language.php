1326daf0fb616581bc41e25154d7f6021e087fbb
Michael Lawrence Evans <mevans@stamen.com>
2012-02-14 01:35:26 +0000

    Correctly calling enforce_master_on_off function, cleaning up code

--- site/www/language.php
+++ site/www/language.php
@@ -7,34 +7,18 @@
     */
 
     ini_set('include_path', ini_get('include_path').PATH_SEPARATOR.'../lib');
-    ini_set('include_path', ini_get('include_path').PATH_SEPARATOR.'/usr/home/migurski/pear/lib');
     require_once 'init.php';
     require_once 'data.php';
     require_once 'lib.auth.php';
-    
-    /*
-    header('Content-Type: text/plain');
-    print_r($_POST);
-    print_r($_SERVER);
-    die();
-    */
-    
-    //list($user_id, $language) = read_userdata($_COOKIE['visitor'], $_SERVER['HTTP_ACCEPT_LANGUAGE']);
-    
-    //enforce_master_on_off_switch($language);
+        
+    $language = $_SERVER['HTTP_ACCEPT_LANGUAGE'];
+      
+    enforce_master_on_off_switch($language);
     
     session_start();
     $dbh =& get_db_connection();
     remember_user($dbh);
     
-    /*
-
-    $dbh =& get_db_connection();
-    
-    $user = $user_id ? get_user($dbh, $user_id) : add_user($dbh);
-    
-    */
-
     if($_POST['language'])
     {
         // change to some other language
@@ -50,11 +34,6 @@
         header("Location: {$location}");
     }
     
-    /*
-    if($user['id'])
-        setcookie('visitor', write_userdata($user['id'], $language), time() + 86400 * 31);
-    */
-
     $sm = get_smarty_instance();
     $sm->assign('language', $language);
     
