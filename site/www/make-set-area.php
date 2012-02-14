067483c8fe37a5ae62a7d63092c013b7f86cbb6f
Michael Lawrence Evans <mevans@stamen.com>
2012-02-14 01:47:20 +0000

    Including call to enforce_master_on_off_switch

--- site/www/make-set-area.php
+++ site/www/make-set-area.php
@@ -4,6 +4,10 @@
     require_once 'data.php';
     require_once 'lib.auth.php';
     require_once 'lib.forms.php';
+    
+    $language = $_SERVER['HTTP_ACCEPT_LANGUAGE'];
+      
+    enforce_master_on_off_switch($language);
 
     session_start();
     $dbh =& get_db_connection();
