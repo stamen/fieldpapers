2bcf9f74c91f44795b1202fa2e2ffc230dd14149
Michael Lawrence Evans <mevans@stamen.com>
2012-02-14 20:13:20 +0000

    Removing language variable

--- site/www/verify.php
+++ site/www/verify.php
@@ -2,10 +2,11 @@
 
     require_once '../lib/lib.everything.php';
     
-    $language = $_SERVER['HTTP_ACCEPT_LANGUAGE'];
-    enforce_master_on_off_switch($language);
+    enforce_master_on_off_switch($_SERVER['HTTP_ACCEPT_LANGUAGE']);
     
     $dbh =& get_db_connection();
+    
+    /**** ... ****/
     
     if ($_GET['email'] && $_GET['hash']) 
     {
