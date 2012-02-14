f65633a67be8584ad2f449f3ee9129c6a627aa02
Michael Lawrence Evans <mevans@stamen.com>
2012-02-14 19:03:35 +0000

    Cleaning up about page and its template

--- site/www/about.php
+++ site/www/about.php
@@ -8,18 +8,15 @@
     require_once 'data.php';
     require_once 'lib.auth.php';
     
-    $language = $_SERVER['HTTP_ACCEPT_LANGUAGE'];
-    
-    enforce_master_on_off_switch($language);
-
-    /**** ... ****/
+    enforce_master_on_off_switch($_SERVER['HTTP_ACCEPT_LANGUAGE']);
     
     session_start();
     $dbh =& get_db_connection();
     remember_user($dbh);
-        
+
+    /**** ... ****/
+            
     $sm = get_smarty_instance();
-    $sm->assign('language', $language);
     
     header("Content-Type: text/html; charset=UTF-8");
     print $sm->fetch("about.html.tpl");
