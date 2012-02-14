df2cce38981ca705c80554df9099f10a67d62522
Michael Lawrence Evans <mevans@stamen.com>
2012-02-14 01:02:19 +0000

    Fixing user auth, cleaning up code

--- site/www/finish-form.php
+++ site/www/finish-form.php
@@ -20,21 +20,13 @@
     // Getting the correct form id
     $form_id = $_GET['id'] ? $_GET['id'] : null;
     
-    //list($user_id, $language) = read_userdata($_COOKIE['visitor'], $_SERVER['HTTP_ACCEPT_LANGUAGE']);
-    
-    //enforce_master_on_off_switch($language);
+    enforce_master_on_off_switch($language);
 
     /**** ... ****/
     
+    session_start();
     $dbh =& get_db_connection();
-    
-    /*
-    if($user_id)
-        $user = get_user($dbh, $user_id);
-
-    if($user)
-        setcookie('visitor', write_userdata($user['id'], $language), time() + 86400 * 31);
-    */
+    remember_user($dbh);
     
     $form = get_form($dbh, $form_id);
     
