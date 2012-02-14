0ded2d368fc260b5a953fab9347d8ed347babd37
Michael Lawrence Evans <mevans@stamen.com>
2012-02-14 19:54:41 +0000

    Removing language variable and creating enforce/session block

--- site/www/print.php
+++ site/www/print.php
@@ -7,18 +7,16 @@
     require_once 'init.php';
     require_once 'data.php';
     require_once 'lib.auth.php';
-    
-    $language = $_SERVER['HTTP_ACCEPT_LANGUAGE'];
       
-    enforce_master_on_off_switch($language);
-    
-    $print_id = $_GET["id"];
-
-    /**** ... ****/
+    enforce_master_on_off_switch($_SERVER['HTTP_ACCEPT_LANGUAGE']);
     
     session_start();
     $dbh =& get_db_connection();
     remember_user($dbh);
+    
+    /**** ... ****/
+    
+    $print_id = $_GET["id"];
 
     $sm = get_smarty_instance();
            
--- site/www/registration.php
+++ site/www/registration.php
@@ -4,17 +4,15 @@
     require_once 'data.php';
     require_once 'lib.auth.php';
     require_once 'output.php';
-    
-    $language = $_SERVER['HTTP_ACCEPT_LANGUAGE'];
       
-    enforce_master_on_off_switch($language);
-    
-    /**** ... ****/
+    enforce_master_on_off_switch($_SERVER['HTTP_ACCEPT_LANGUAGE']);
     
     session_start();
     $dbh =& get_db_connection();
     remember_user($dbh);
     
+    /**** ... ****/
+        
     $sm = get_smarty_instance();
            
     switch($_POST['action'])
