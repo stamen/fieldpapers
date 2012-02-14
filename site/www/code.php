f9662907479e0c92c77ba4eec21a8c893788c056
Michael Lawrence Evans <mevans@stamen.com>
2012-02-14 19:33:28 +0000

    Removing language variable

--- site/www/code.php
+++ site/www/code.php
@@ -4,9 +4,9 @@
     require_once 'qrcode.php';
     require_once 'output.php';
     
-    $language = $_SERVER['HTTP_ACCEPT_LANGUAGE'];
+    enforce_master_on_off_switch($_SERVER['HTTP_ACCEPT_LANGUAGE']);
     
-    enforce_master_on_off_switch($language);
+    /**** ... ****/
     
     $url = 'http://'.get_domain_name().get_base_dir().'/print.php?id='.urlencode($_GET['print']);
     $qrc = QRCode::getMinimumQRCode($url, QR_ERROR_CORRECT_LEVEL_Q);
