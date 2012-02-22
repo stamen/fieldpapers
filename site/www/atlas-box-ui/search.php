ff2f231ba1fb89c3d74303c3a567322d6066ca09
Michael Lawrence Evans <mevans@stamen.com>
2012-02-22 07:49:37 +0000

    Removing comments

--- site/www/atlas-box-ui/search.php
+++ site/www/atlas-box-ui/search.php
@@ -10,13 +10,10 @@
     
     /**** ... ****/
     
-    //print_r(GEOPLANET_APPID);
     $sm = get_smarty_instance();
     $sm->assign('app_id', GEOPLANET_APPID);
     
     header("Content-Type: text/html; charset=UTF-8");
     print $sm->fetch("search.html.tpl");
     
-    //$mbtiles_url = 'http://'.get_domain_name().get_base_dir().'/display_mbtiles.php?filename='.urlencode($slug);
-    //header("Location: http://fieldpapers.org/~mevans/fieldpapers/site/www/atlas-box-ui/new-box-ui.php");
 ?>
