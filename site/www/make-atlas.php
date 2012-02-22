a9810dcff2433284338a3bc5308b22d340f726aa
Michael Lawrence Evans <mevans@stamen.com>
2012-02-22 23:23:45 +0000

    Moving new atlas creation to the main www directory; renaming atlas creation pages

---
+++ site/www/make-atlas.php
@@ -0,0 +1,24 @@
+<?php
+
+    require_once '../lib/lib.everything.php';
+
+    enforce_master_on_off_switch($_SERVER['HTTP_ACCEPT_LANGUAGE']);
+
+    session_start();
+    $dbh =& get_db_connection();
+    remember_user($dbh);
+    
+    /**** ... ****/
+    
+    $extent = array('ne' => $_GET['ne'], 'sw' => $_GET['sw']);
+    $center = $_GET['center'];
+        
+    $sm = get_smarty_instance();
+    $sm->assign('forms', $forms);
+    $sm->assign('default_form', $default_form);
+    $sm-> assign('extent', $extent);
+    $sm-> assign('center', $center);
+    
+    header("Content-Type: text/html; charset=UTF-8");
+    print $sm->fetch("make-atlas.html.tpl");
+?>
