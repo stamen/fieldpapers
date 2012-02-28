e83d29a71719909cf07b7e16a917272086e2b170
Michael Lawrence Evans <mevans@stamen.com>
2012-02-28 22:29:40 +0000

    Rewriting how we deal with individual print pages

--- site/www/print.php
+++ site/www/print.php
@@ -12,68 +12,41 @@
     
     /**** ... ****/
     
-    $print_id = $_GET['id'] ? $_GET['id'] : null; 
+    $print_id = $_GET['id'] ? $_GET['id'] : null;
     
-    if(preg_match('#^([0-9a-zA-Z]+)+/(\d+)$#', $print_id, $matches))
-    {
-        $print_id = $matches[1];
-        $page_number = $matches[2];
-    }
+    $print = get_print($dbh,$print_id);
+        
+    $sm = get_smarty_instance();
     
-    if ($print_id && $page_number)
+    if ($print['selected_page'])
     {
-        $sm = get_smarty_instance();
-        $sm->assign('flickr_key', FLICKR_KEY);
-        
-        $print = get_print($dbh, $print_id);
-        $sm->assign('print', $print);
-        $sm->assign('place_id', $print['place_woeid']); 
-        
-        $zoom = 8; //Zoom should be in the database
-        
-        $place = latlon_placeinfo($print['north'], $print['west'], $zoom);
-        $sm->assign('place', $place);
-        
-        $user = get_user($dbh, $print['user_id']);
-        
-        if ($user['name'])
-        {
-            $sm->assign('user_name', $user['name']);
-        } else {
-            $sm->assign('user_name', 'Anonymous');
-        }
-        
-        $page = get_print_page($dbh, $print_id, $page_number);
-        $sm->assign('page',$page);
-        
-        // For GeoJSON output
-        $pages = array($page);
-    } else if ($print_id) {
-        $sm = get_smarty_instance();
-        $sm->assign('flickr_key', FLICKR_KEY);
-                   
-        $print = get_print($dbh, $print_id);
-        $sm->assign('print', $print);
-        $sm->assign('place_id', $print['place_woeid']);
-        
-        $zoom = 8; //Zoom should be in the database
-        
-        $place = latlon_placeinfo($print['north'], $print['west'], $zoom);
-        $sm->assign('place', $place);
-        
-        $user = get_user($dbh, $print['user_id']);
-        
-        if ($user['name'])
-        {
-            $sm->assign('user_name', $user['name']);
-        } else {
-            $sm->assign('user_name', 'Anonymous');
-        }
-        
+        $pages = array($print['selected_page']);
+    } else {
         $pages = get_print_pages($dbh, $print_id);
-        $sm->assign('pages', $pages);
     }
     
+    $sm->assign('pages', $pages);
+    
+    $sm->assign('flickr_key', FLICKR_KEY);
+    
+    $sm->assign('print', $print);
+    $sm->assign('place_id', $print['place_woeid']); 
+    
+    $zoom = 8; //Zoom should be in the database
+    
+    $place = latlon_placeinfo(.5 * ($print['north'] + $print['south']), 
+                              .5 * ($print['west'] + $print['east']), $zoom);
+    $sm->assign('place', $place);
+    
+    $user = get_user($dbh, $print['user_id']);
+    
+    if ($user['name'])
+    {
+        $sm->assign('user_name', $user['name']);
+    } else {
+        $sm->assign('user_name', 'Anonymous');
+    }
+        
     $type = $_GET['type'] ? $_GET['type'] : $_SERVER['HTTP_ACCEPT'];
     $type = get_preferred_type($type);
     
