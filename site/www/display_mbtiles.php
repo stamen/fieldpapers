253b7a0751c0034fd8b7d607c5bc5b1f86d3b157
Michael Lawrence Evans <mevans@stamen.com>
2012-03-09 18:54:09 +0000

    Including id in the GET variable; passing mbtiles data to the template

--- site/www/display_mbtiles.php
+++ site/www/display_mbtiles.php
@@ -5,9 +5,14 @@
     $dbh =& get_db_connection();
     remember_user($dbh);
     
-    $filename = $_GET['filename'];
-        
+    $filename = $_GET['filename'] ? $_GET['filename'] : null;
+    $id = $_GET['id'] ? $_GET['id'] : null;
+    
+    $mbtiles_data = get_mbtiles_by_id(&$dbh, $id);
+    
     $sm = get_smarty_instance();
     $sm->assign('filename', $filename);
+    $sm->assign('mbtiles_data', $mbtiles_data);
+    
     print $sm->fetch("display_mbtiles.html.tpl");
 ?>
