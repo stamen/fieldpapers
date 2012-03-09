c6b5acbeffa73a153ec69070dd623743bff7ff91
Michael Lawrence Evans <mevans@stamen.com>
2012-03-09 06:14:15 +0000

    Accessing user information and passing it to the upload_mbtiles template

--- site/www/upload_mbtiles.php
+++ site/www/upload_mbtiles.php
@@ -1,13 +1,14 @@
 <?php
-    ini_set('include_path', ini_get('include_path').PATH_SEPARATOR.'../lib');
-    require_once 'init.php';
-    require_once 'data.php';
-    require_once 'lib.auth.php';
+    require_once '../lib/lib.everything.php';
 
     session_start();
     $dbh =& get_db_connection();
     remember_user($dbh);
+    
+    $user = cookied_user($dbh);
         
     $sm = get_smarty_instance();
+    $sm->assign('user', $user);
+    
     print $sm->fetch("upload_mbtiles.html.tpl");
 ?>
