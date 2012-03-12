034d9078a42b252470bc8dc13707c96857cbcdd5
Michael Lawrence Evans <mevans@stamen.com>
2012-03-12 06:18:02 +0000

    Passing possible mbtiles data to the make-atlas template

--- site/www/make-atlas.php
+++ site/www/make-atlas.php
@@ -9,15 +9,31 @@
     remember_user($dbh);
     
     /**** ... ****/
-    $extent = array('ne' => $_GET['ne'], 'sw' => $_GET['sw']);
-    $center = $_GET['center'];
-        
-    $sm = get_smarty_instance();
-    $sm->assign('forms', $forms);
-    $sm->assign('default_form', $default_form);
-    $sm-> assign('extent', $extent);
-    $sm-> assign('center', $center);
     
+    $sm = get_smarty_instance();
+        
+    // Used to use POST variable
+    if ($_GET['mbtiles_id'])
+    {        
+        $mbtiles = get_mbtiles_by_id($dbh, $_GET['mbtiles_id']);
+                
+        $mbtiles_data = array("provider" =>      $mbtiles['url'],
+                              "uploaded_file" => $mbtiles['uploaded_file'],
+                              "center_x" =>      $mbtiles['center_x_coord'],
+                              "center_y" =>      $mbtiles['center_y_coord'],
+                              "zoom" =>          $mbtiles['center_zoom'],
+                              'min_zoom' =>      $mbtiles['min_zoom'],
+                              'max_zoom' =>      $mbtiles['max_zoom']
+                              );
+        $sm-> assign('mbtiles_data', $mbtiles_data); 
+    } else {
+        $center = $_GET['center'];
+        $zoom = 10;
+        $sm-> assign('center', $center);
+        $sm-> assign('zoom', $zoom);
+    }
+        
     header("Content-Type: text/html; charset=UTF-8");
     print $sm->fetch("make-atlas.html.tpl");
+    
 ?>
