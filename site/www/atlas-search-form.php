dc0627208852e755ec6c97862fd053e025547947
Michael Lawrence Evans <mevans@stamen.com>
2012-03-12 06:03:33 +0000

    Retrieving mbtiles data for each user for display on the atlas search form

--- site/www/atlas-search-form.php
+++ site/www/atlas-search-form.php
@@ -1,5 +1,4 @@
 <?php
-
     require_once '../lib/lib.everything.php';
 
     enforce_master_on_off_switch($_SERVER['HTTP_ACCEPT_LANGUAGE']);
@@ -10,8 +9,21 @@
     
     /**** ... ****/
     
+    $user = cookied_user($dbh);
+    
     $sm = get_smarty_instance();
-    $sm->assign('app_id', GEOPLANET_APPID);
+    
+    if($_GET['error'] == 'no_response')
+    {
+        $sm->assign('error', $_GET['error']);
+    }
+    
+    $user_mbtiles = get_mbtiles_by_user_id($dbh, $user['id']);
+    
+    if ($user_mbtiles)
+    {
+        $sm->assign('user_mbtiles', $user_mbtiles);
+    }
     
     header("Content-Type: text/html; charset=UTF-8");
     print $sm->fetch("atlas-search-form.html.tpl");
