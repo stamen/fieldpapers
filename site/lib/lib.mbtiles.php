e05b64405afc057144e5a978ada90527a71bae7c
Michael Lawrence Evans <mevans@stamen.com>
2012-03-12 06:16:04 +0000

    Changing get_mbtiles_data function to query an mbtiles file for its center coordinates per zoom level and its minimum/maximum zoom levels; changing add_mbtiles function to insert an mbtiles' center coordinates into the database; changing get_mbtiles functions to query based on an mbtiles' center coordinates ; commenting out extract_mbtiles_metadata function

--- site/lib/lib.mbtiles.php
+++ site/lib/lib.mbtiles.php
@@ -2,39 +2,31 @@
 
     require_once 'data.php';
 
-    function add_mbtiles(&$dbh, $user_id, $url, $file_path)
+    function add_mbtiles(&$dbh, $user_id, $url, $file_name, $file_path)
     {
-        $metadata = extract_mbtiles_metadata($file_path);
-                
+        $mbtiles_data = get_mbtiles_data($file_path);
+                        
         $mbtiles_id = generate_id();
-        
-        // Check type for query
         
         $q = sprintf('INSERT INTO mbtiles
                       SET id = %s, 
                       user_id = %s,
                       url = %s,
-                      uploaded_file_path = %s,
-                      min_zoom=%s,
-                      max_zoom=%s,
-                      north=%s,
-                      south=%s,
-                      east=%s,
-                      west=%s,
-                      center_lat=%s,
-                      center_lon=%s',
+                      uploaded_file = %s,
+                      min_zoom = %d,
+                      max_zoom = %d,
+                      center_zoom = %d,
+                      center_x_coord = %d,
+                      center_y_coord = %d',
                      $dbh->quoteSmart($mbtiles_id),
                      $dbh->quoteSmart($user_id),
                      $dbh->quoteSmart($url),
-                     $dbh->quoteSmart($file_path),
-                     $dbh->quoteSmart($metadata['min_zoom']),
-                     $dbh->quoteSmart($metadata['max_zoom']),
-                     $dbh->quoteSmart($metadata['north']),
-                     $dbh->quoteSmart($metadata['south']),
-                     $dbh->quoteSmart($metadata['east']),
-                     $dbh->quoteSmart($metadata['west']),
-                     $dbh->quoteSmart($metadata['center_lat']),
-                     $dbh->quoteSmart($metadata['center_lon']));
+                     $dbh->quoteSmart($file_name),
+                     $dbh->quoteSmart($mbtiles_data['min_zoom']),
+                     $dbh->quoteSmart($mbtiles_data['max_zoom']),
+                     $dbh->quoteSmart($mbtiles_data["center_coordinates"]['zoom']),
+                     $dbh->quoteSmart($mbtiles_data["center_coordinates"]['x']),
+                     $dbh->quoteSmart($mbtiles_data["center_coordinates"]['y']));
                      
         error_log(preg_replace('/\s+/', ' ', $q));
 
@@ -51,11 +43,55 @@
         return get_mbtiles_by_id($dbh, $mbtiles_id);
     }
     
+    function get_mbtiles_data($file_path)
+    {
+        $db_mbtiles = new SQLite3($file_path);
+        
+        // Min Zoom and Max Zoom
+        
+        $zoom_range_query = "select min(zoom_level), max(zoom_level) from tiles";
+        $zoom_range_results = $db_mbtiles->query($zoom_range_query);
+        
+        $zoom_range = $zoom_range_results->fetchArray();
+        
+        $min_zoom = $zoom_range[0];
+        $max_zoom = $zoom_range[1];
+        
+        // Get zoom, column, and row for each tile
+        
+        $query = "select zoom_level, avg(tile_column), avg(tile_row) from tiles group by zoom_level";
+        $results = $db_mbtiles->query($query);
+        
+        $rows = array();
+        
+        while ($row = $results->fetchArray())
+        {
+            $rows[] = $row;
+        }
+        
+        $center_tile = $rows[floor(.5*count($rows))];
+                
+        $center_tile_zoom = $center_tile[0];
+        $center_tile_x = floor($center_tile[1]); // Column
+        $center_tile_y = round(pow(2,$center_tile_zoom) - $center_tile[2] - 1); // Converted Row
+        
+        $mbtiles_data = array("min_zoom" => $min_zoom,
+                              "max_zoom" => $max_zoom,
+                              "center_coordinates" => array("zoom" => $center_tile_zoom,
+                                                            "x" => intval($center_tile_x),
+                                                            "y" => intval($center_tile_y)
+                                                            )
+                            );
+                
+        return $mbtiles_data;
+    }
+    
+    /*
     function extract_mbtiles_metadata($file_path)
     {
         $db_mbtiles = new SQLite3($file_path);
 
-        /*Query the SQLite database for metadata.*/
+        // Query the SQLite database for metadata.
                 
         $minzoom_query = "select value from metadata where name='minzoom'";
         $minzoom = $db_mbtiles->querySingle($minzoom_query);
@@ -75,7 +111,6 @@
                 
         $bounds_array = explode(',', $bounds);
             
-        /* See MBTiles Spec: https://github.com/mapbox/mbtiles-spec/blob/master/1.2/spec.md */
         $west = $bounds_array[0];
         $south = $bounds_array[1];
         $east = $bounds_array[2];
@@ -94,12 +129,14 @@
         
         return $metadata;
     }
+    */
     
     function get_mbtiles_by_id(&$dbh, $id)
     {   
         $q = sprintf("SELECT id, user_id, created,
-                             is_private, url, uploaded_file_path,
-                             min_zoom, max_zoom, north, south, east, west
+                             is_private, url, uploaded_file,
+                             min_zoom, max_zoom, center_zoom,
+                             center_x_coord, center_y_coord
                       FROM mbtiles
                       WHERE id=%s",
                       $dbh->quoteSmart($id));
@@ -124,10 +161,12 @@
     function get_mbtiles_by_user_id(&$dbh, $user_id)
     {   
         $q = sprintf("SELECT id, user_id, created,
-                             is_private, url, uploaded_file_path,
-                             min_zoom, max_zoom, north, south, east, west
+                             is_private, url, uploaded_file,
+                             min_zoom, max_zoom, center_zoom,
+                             center_x_coord, center_y_coord
                       FROM mbtiles
-                      WHERE user_id=%s",
+                      WHERE user_id=%s
+                      ORDER BY uploaded_file ASC",
                       $dbh->quoteSmart($user_id));
                      
         error_log(preg_replace('/\s+/', ' ', $q));
