d96a370396899f44a1ff037c8106357ee5184393
Michael Lawrence Evans <mevans@stamen.com>
2012-03-27 03:59:56 +0000

    Separating out JavaScript for the map on the snapshot page

---
+++ site/www/js/snapshot_map.js
@@ -0,0 +1,24 @@
+var zoom_in_active = base_url + '/img/button-zoom-in-on.png',
+    zoom_in_inactive = base_url + '/img/button-zoom-in-off.png',
+    zoom_out_active = base_url + '/img/button-zoom-out-on.png',
+    zoom_out_inactive = base_url + '/img/button-zoom-out-off.png';
+
+var map;
+var MM;    
+    
+    MM = com.modestmaps;
+    var provider = base_provider + '/{Z}/{X}/{Y}.jpg';
+    map = new MM.Map("map", new MM.TemplatedMapProvider(provider), null, [new MM.DragHandler(), new MM.DoubleClickHandler()]);
+    
+var bounds = geojpeg_bounds.split(','),
+    north = parseFloat(bounds[0]),
+    west = parseFloat(bounds[1]),
+    south = parseFloat(bounds[2]),
+    east = parseFloat(bounds[3]),
+    extents = [new MM.Location(north, west), new MM.Location(south, east)];
+
+map.setExtent(extents);
+map.zoomIn();
+
+document.getElementById('zoom-out').style.display = 'inline';
+document.getElementById('zoom-in').style.display = 'inline';
