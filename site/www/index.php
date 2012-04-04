a976bbae35b799987190cdd8ff348a39cdf90069
Michael Lawrence Evans <mevans@stamen.com>
2012-04-04 18:31:01 +0000

    Removing unused and unncecessary code from index.php

--- site/www/index.php
+++ site/www/index.php
@@ -7,24 +7,7 @@
     $context = default_context();
 
     /**** ... ****/
-    
-    $prints = get_prints($context->db, null, 6);
-    
-    // Get user names
-    foreach ($prints as $i => $print)
-    {
-        $user = get_user($context->db, $prints[$i]['user_id']);
         
-        if ($user['name'])
-        {
-            $prints[$i]['user_name'] = $user['name'];
-        } else {
-            $prints[$i]['user_name'] = 'Anonymous';
-        }
-    }
-    
-    $context->sm->assign('prints', $prints);
-    
     header("Content-Type: text/html; charset=UTF-8");
     print $context->sm->fetch("index.html.tpl");
 
