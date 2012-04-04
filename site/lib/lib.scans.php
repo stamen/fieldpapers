f16068d056e78616d5941d84e649b0c7c93a7cf4
Michael Lawrence Evans <mevans@stamen.com>
2012-04-04 01:50:32 +0000

    Merging lib.scans; removing get_all_scan_notes function

--- site/lib/lib.scans.php
+++ site/lib/lib.scans.php
@@ -297,7 +297,7 @@
         return $row;   
     }
     
-    function get_scan_notes(&$dbh, $args, $page)
+    function get_scan_notes(&$dbh, $args, $page=9999)
     {
         list($count, $offset, $perpage, $page) = get_pagination($page);
         
@@ -330,31 +330,6 @@
                       LIMIT %d OFFSET %d',
                      join(' AND ', $where_clauses),
                      $count, $offset);
-    
-        $res = $dbh->query($q);
-        
-        if(PEAR::isError($res)) 
-            die_with_code(500, "{$res->message}\n{$q}\n");
-
-        $rows = array();
-        
-        while($row = $res->fetchRow(DB_FETCHMODE_ASSOC))
-            $rows[] = $row;
-        
-        return $rows;
-    }
-    
-    function get_all_scan_notes(&$dbh, $scan_id)
-    {        
-        $q = sprintf('SELECT scan_id, note_number, note,
-                             latitude, longitude, geometry,
-                             UNIX_TIMESTAMP(created) AS created,
-                             UNIX_TIMESTAMP(NOW()) - UNIX_TIMESTAMP(created) AS age,
-                             user_id
-                      FROM scan_notes
-                      WHERE scan_id=%s
-                      ORDER BY created DESC',
-                     $dbh->quoteSmart($scan_id));
     
         $res = $dbh->query($q);
         
@@ -441,50 +416,6 @@
         {
             die_with_code(500, "{$res->message}\n{$q}\n");
         }
-    }
-    
-    function scan_notes_to_geojson($notes)
-    {
-        $geojson = array(
-            'type' => 'FeatureCollection',
-            'features' => array()
-        );
-        
-        $base_href = 'http://'.get_domain_name().get_base_dir();
-        
-        foreach($notes as $note)
-        {
-            $scan = $note['scan'];
-        
-            $feature = array(
-                'type' => 'Feature',
-                'id' => "{$note['scan_id']}/{$note['note_number']}",
-                'properties' => array(
-                    'note' => $note['note'],
-                    'number' => intval($note['note_number']),
-                    'created' => date('c', $note['created']),
-
-                    'scan_id' => $note['scan_id'],
-                    'scan_href' => "{$base_href}/snapshot.php?id={$note['scan_id']}",
-                    'scan_description' => $scan['description'],
-                    'scan_place_woeid' => $scan['place_woeid'],
-                    'scan_region_woeid' => $scan['region_woeid'],
-                    'scan_country_woeid' => $scan['country_woeid'],
-                    'scan_place_name' => $scan['place_name'],
-                    'scan_region_name' => $scan['region_name'],
-                    'scan_country_name' => $scan['country_name'],
-                    'scan_created' => date('c', $scan['created'])
-                ),
-                'geometry' => array(
-                    'type' => 'Point',
-                    'coordinates' => array(floatval($note['longitude']), floatval($note['latitude']))
-                )
-            );
-            
-            $geojson['features'][] = $feature;
-        }
-        
-        return json_encode($geojson);
     }
     
     function scan_to_geojson_feature($scan)
