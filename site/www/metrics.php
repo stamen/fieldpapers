c9bb6a7999dd3fde2cca0b94760c27d5be157d06
Seth Fitzsimmons <seth@mojodna.net>
2013-04-26 21:34:45 +0000

    Rudimentary metrics, pass 1

---
+++ site/www/metrics.php
@@ -0,0 +1,37 @@
+<?php
+
+require_once '../lib/lib.everything.php';
+
+enforce_master_on_off_switch($_SERVER['HTTP_ACCEPT_LANGUAGE']);
+
+$context = default_context(false);
+
+header("Content-Type: application/json");
+
+$query = <<<EOQ
+SELECT
+    COUNT(pages.print_id) AS pages,
+    prints.created,
+    prints.composed
+FROM prints
+LEFT JOIN pages ON pages.print_id=prints.id
+GROUP BY prints.id
+ORDER BY created DESC
+EOQ;
+
+$res = $context->db->query($query);
+
+if (PEAR::isError($res)) {
+    die_with_code(500, $res->message);
+}
+
+$rsp = array();
+
+while ($row = $res->fetchRow(DB_FETCHMODE_ASSOC)) {
+    $rsp[] = $row;
+}
+$context->close();
+
+echo json_encode($rsp);
+
+?>
