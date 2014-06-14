984a85e7a103edbd85a6e4593c39684a392a838a
Brendan Collins <bcollins@blueraster.com>
2014-06-14 00:40:20 -0400

    removed call-time pass-by-reference after getting deprecation error post mbtiles upload

--- site/www/display_mbtiles.php
+++ site/www/display_mbtiles.php
@@ -7,7 +7,7 @@
     $filename = $_GET['filename'] ? $_GET['filename'] : null;
     $id = $_GET['id'] ? $_GET['id'] : null;
     
-    $mbtiles_data = get_mbtiles_by_id(&$context->db, $id);
+    $mbtiles_data = get_mbtiles_by_id($context->db, $id);
     
     $context->sm->assign('filename', $filename);
     $context->sm->assign('mbtiles_data', $mbtiles_data);
