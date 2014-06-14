b82efc3ead0313af8f5d93cff5c28bcf3b00e3de
Brendan Collins <bcollins@blueraster.com>
2014-06-14 09:04:31 -0400

    moved advanced tool section from about page to top-level navigation

---
+++ site/www/advanced.php
@@ -0,0 +1,17 @@
+<?php
+   /**
+    * Basic, functionless about page.
+    */
+
+    require_once '../lib/lib.everything.php';
+    
+    enforce_master_on_off_switch($_SERVER['HTTP_ACCEPT_LANGUAGE']);
+    
+    $context = default_context(True);
+
+    /**** ... ****/
+            
+    header("Content-Type: text/html; charset=UTF-8");
+    print $context->sm->fetch("advanced.html.tpl");
+
+?>
